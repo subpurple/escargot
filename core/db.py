136fb13a5609ab33b9af550281722ecedd6f23c8
valtron <valtron2000@gmail.com>
2019-04-18 23:54:53 -0600

    shortcut for `sa.Column(nullable = False)`

--- core/db.py
+++ core/db.py
@@ -14,24 +14,29 @@ import settings
 class Base(declarative_base()): # type: ignore
 	__abstract__ = True
 
+def Col(t: sa.types.TypeEngine, **kwargs: Any) -> sa.Column:
+	if 'nullable' not in kwargs:
+		kwargs['nullable'] = False
+	return sa.Column(t, **kwargs)
+
 class User(Base):
 	__tablename__ = 't_user'
 	
-	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-	date_created = sa.Column(sa.DateTime, nullable = True, default = datetime.utcnow)
-	date_login = sa.Column(sa.DateTime, nullable = True)
-	uuid = sa.Column(sa.String, nullable = False, unique = True)
-	email = sa.Column(sa.String, nullable = False)
-	relay = sa.Column(sa.Boolean, nullable = False, default = False)
-	verified = sa.Column(sa.Boolean, nullable = False)
-	name = sa.Column(sa.String, nullable = True)
-	message = sa.Column(sa.String, nullable = False)
-	password = sa.Column(sa.String, nullable = False)
-	settings = sa.Column(JSONType, nullable = False)
+	id = Col(sa.Integer, primary_key = True)
+	date_created = Col(sa.DateTime, nullable = True, default = datetime.utcnow)
+	date_login = Col(sa.DateTime, nullable = True)
+	uuid = Col(sa.String, unique = True)
+	email = Col(sa.String)
+	relay = Col(sa.Boolean, default = False)
+	verified = Col(sa.Boolean)
+	name = Col(sa.String, nullable = True)
+	message = Col(sa.String)
+	password = Col(sa.String)
+	settings = Col(JSONType)
 	
 	# Data specific to front-ends; e.g. different types of password hashes
 	# E.g. front_data = { 'msn': { ... }, 'ymsg': { ... }, ... }
-	_front_data = sa.Column(JSONType, name = 'front_data', nullable = False, default = {})
+	_front_data = Col(JSONType, name = 'front_data', default = {})
 	
 	def set_front_data(self, frontend: str, key: str, value: Any) -> None:
 		fd = self._front_data or {}
@@ -53,107 +58,107 @@ class User(Base):
 class UserGroup(Base):
 	__tablename__ = 't_user_group'
 	
-	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-	user_uuid = sa.Column(sa.String, nullable = False)
-	group_id = sa.Column(sa.String, nullable = False)
-	group_uuid = sa.Column(sa.String, nullable = False)
-	name = sa.Column(sa.String, nullable = False)
-	is_favorite = sa.Column(sa.Boolean, nullable = False, default = False)
-	date_last_modified = sa.Column(sa.DateTime, nullable = True, default = datetime.utcnow)
+	id = Col(sa.Integer, primary_key = True)
+	user_uuid = Col(sa.String)
+	group_id = Col(sa.String)
+	group_uuid = Col(sa.String)
+	name = Col(sa.String)
+	is_favorite = Col(sa.Boolean, default = False)
+	date_last_modified = Col(sa.DateTime, nullable = True, default = datetime.utcnow)
 
 class UserContact(Base):
 	__tablename__ = 't_user_contact'
 	
-	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-	user_uuid = sa.Column(sa.String, nullable = False)
-	uuid = sa.Column(sa.String, nullable = False)
-	name = sa.Column(sa.String, nullable = True)
-	message = sa.Column(sa.String, nullable = True)
-	lists = sa.Column(sa.Integer, nullable = False)
-	groups = sa.Column(JSONType, nullable = False)
+	id = Col(sa.Integer, primary_key = True)
+	user_uuid = Col(sa.String)
+	uuid = Col(sa.String)
+	name = Col(sa.String, nullable = True)
+	message = Col(sa.String, nullable = True)
+	lists = Col(sa.Integer)
+	groups = Col(JSONType)
 
 class AddressBook(Base):
 	__tablename__ = 't_addressbook'
 	
-	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-	member_uuid = sa.Column(sa.String, nullable = False)
-	date_created = sa.Column(sa.DateTime, nullable = True, default = datetime.utcnow)
-	date_last_modified = sa.Column(sa.DateTime, nullable = True)
+	id = Col(sa.Integer, primary_key = True)
+	member_uuid = Col(sa.String)
+	date_created = Col(sa.DateTime, nullable = True, default = datetime.utcnow)
+	date_last_modified = Col(sa.DateTime, nullable = True)
 
 class AddressBookContact(Base):
 	__tablename__ = 't_addressbook_contact'
 	
-	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-	ab_origin_uuid = sa.Column(sa.String, nullable = False)
-	contact_id = sa.Column(sa.String, nullable = False)
+	id = Col(sa.Integer, primary_key = True)
+	ab_origin_uuid = Col(sa.String)
+	contact_id = Col(sa.String)
 	# `contact_uuid` is a UUID that identifies the contact in the addressbook; unrelated to the UUID of the contact's account
 	# `contact_member_uuid` is the contact's account's UUID, indicating that the contact's a part of our network.
-	contact_uuid = sa.Column(sa.String, nullable = False)
-	contact_member_uuid = sa.Column(sa.String, nullable = True)
-	date_last_modified = sa.Column(sa.DateTime, nullable = True, default = datetime.utcnow)
-	type = sa.Column(sa.String, nullable = False)
-	email = sa.Column(sa.String, nullable = False)
-	birthdate = sa.Column(sa.DateTime, nullable = True)
-	anniversary = sa.Column(sa.DateTime, nullable = True)
-	notes = sa.Column(sa.String, nullable = True)
-	name = sa.Column(sa.String, nullable = True)
-	first_name = sa.Column(sa.String, nullable = True)
-	middle_name = sa.Column(sa.String, nullable = True)
-	last_name = sa.Column(sa.String, nullable = True)
-	nickname = sa.Column(sa.String, nullable = True)
-	primary_email_type = sa.Column(sa.String, nullable = True)
-	personal_email = sa.Column(sa.String, nullable = True)
-	work_email = sa.Column(sa.String, nullable = True)
-	im_email = sa.Column(sa.String, nullable = True)
-	other_email = sa.Column(sa.String, nullable = True)
-	home_phone = sa.Column(sa.String, nullable = True)
-	work_phone = sa.Column(sa.String, nullable = True)
-	fax_phone = sa.Column(sa.String, nullable = True)
-	pager_phone = sa.Column(sa.String, nullable = True)
-	mobile_phone = sa.Column(sa.String, nullable = True)
-	other_phone = sa.Column(sa.String, nullable = True)
-	personal_website = sa.Column(sa.String, nullable = True)
-	business_website = sa.Column(sa.String, nullable = True)
-	groups = sa.Column(JSONType, nullable = False)
-	is_messenger_user = sa.Column(sa.Boolean, nullable = False, default = False)
+	contact_uuid = Col(sa.String)
+	contact_member_uuid = Col(sa.String, nullable = True)
+	date_last_modified = Col(sa.DateTime, nullable = True, default = datetime.utcnow)
+	type = Col(sa.String)
+	email = Col(sa.String)
+	birthdate = Col(sa.DateTime, nullable = True)
+	anniversary = Col(sa.DateTime, nullable = True)
+	notes = Col(sa.String, nullable = True)
+	name = Col(sa.String, nullable = True)
+	first_name = Col(sa.String, nullable = True)
+	middle_name = Col(sa.String, nullable = True)
+	last_name = Col(sa.String, nullable = True)
+	nickname = Col(sa.String, nullable = True)
+	primary_email_type = Col(sa.String, nullable = True)
+	personal_email = Col(sa.String, nullable = True)
+	work_email = Col(sa.String, nullable = True)
+	im_email = Col(sa.String, nullable = True)
+	other_email = Col(sa.String, nullable = True)
+	home_phone = Col(sa.String, nullable = True)
+	work_phone = Col(sa.String, nullable = True)
+	fax_phone = Col(sa.String, nullable = True)
+	pager_phone = Col(sa.String, nullable = True)
+	mobile_phone = Col(sa.String, nullable = True)
+	other_phone = Col(sa.String, nullable = True)
+	personal_website = Col(sa.String, nullable = True)
+	business_website = Col(sa.String, nullable = True)
+	groups = Col(JSONType)
+	is_messenger_user = Col(sa.Boolean, default = False)
 	# annotations = { "Annotation.Name": "Value", ... }
-	annotations = sa.Column(JSONType, nullable = False, default = {})
+	annotations = Col(JSONType, default = {})
 
 class AddressBookContactLocation(Base):
 	__tablename__ = 't_addressbook_contact_location'
 	
-	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-	contact_uuid = sa.Column(sa.String, nullable = False)
-	ab_origin_uuid = sa.Column(sa.String, nullable = False)
-	location_type = sa.Column(sa.String, nullable = False)
-	name = sa.Column(sa.String, nullable = True)
-	street = sa.Column(sa.String, nullable = True)
-	city = sa.Column(sa.String, nullable = True)
-	state = sa.Column(sa.String, nullable = True)
-	country = sa.Column(sa.String, nullable = True)
-	zip_code = sa.Column(sa.String, nullable = True)
+	id = Col(sa.Integer, primary_key = True)
+	contact_uuid = Col(sa.String)
+	ab_origin_uuid = Col(sa.String)
+	location_type = Col(sa.String)
+	name = Col(sa.String, nullable = True)
+	street = Col(sa.String, nullable = True)
+	city = Col(sa.String, nullable = True)
+	state = Col(sa.String, nullable = True)
+	country = Col(sa.String, nullable = True)
+	zip_code = Col(sa.String, nullable = True)
 
 #class CircleStore(Base):
 #	__tablename__ = 't_circle_store'
 #	
-#	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-#	circle_id = sa.Column(sa.String, nullable = False, unique = True)
-#	circle_name = sa.Column(sa.String, nullable = False)
-#	owner_email = sa.Column(sa.String, nullable = False)
-#	owner_friendly = sa.Column(sa.String, nullable = False)
-#	membership_access = sa.Column(sa.Integer, nullable = False)
-#	request_membership_option = sa.Column(sa.Integer, nullable = False)
-#	is_presence_enabled = sa.Column(sa.Boolean, nullable = False)
-#	date_last_modified = sa.Column(sa.DateTime, nullable = False, default = datetime.now)
+#	id = Col(sa.Integer, primary_key = True)
+#	circle_id = Col(sa.String, unique = True)
+#	circle_name = Col(sa.String)
+#	owner_email = Col(sa.String)
+#	owner_friendly = Col(sa.String)
+#	membership_access = Col(sa.Integer)
+#	request_membership_option = Col(sa.Integer)
+#	is_presence_enabled = Col(sa.Boolean)
+#	date_last_modified = Col(sa.DateTime, default = datetime.now)
 
 #class CircleMembership(Base):
 #	__tablename__ = 't_circle_membership'
 #	
-#	id = sa.Column(sa.Integer, nullable = False, primary_key = True)
-#	circle_id = sa.Column(sa.String, nullable = False)
-#	member_email = sa.Column(sa.String, nullable = False)
-#	member_role = sa.Column(sa.Integer, nullable = False)
-#	member_state = sa.Column(sa.Integer, nullable = False)
+#	id = Col(sa.Integer, primary_key = True)
+#	circle_id = Col(sa.String)
+#	member_email = Col(sa.String)
+#	member_role = Col(sa.Integer)
+#	member_state = Col(sa.Integer)
 
 def _simplify_json_data(data: Any) -> Any:
 	if isinstance(data, dict):
@@ -172,11 +177,11 @@ def _simplify_json_data(data: Any) -> Any:
 class Sound(Base):
 	__tablename__ = 't_sound'
 	
-	hash = sa.Column(sa.String, nullable = False, primary_key = True)
-	title = sa.Column(sa.String, nullable = False)
-	category = sa.Column(sa.Integer, nullable = False)
-	language = sa.Column(sa.Integer, nullable = False)
-	is_public = sa.Column(sa.Boolean, nullable = False)
+	hash = Col(sa.String, primary_key = True)
+	title = Col(sa.String)
+	category = Col(sa.Integer)
+	language = Col(sa.Integer)
+	is_public = Col(sa.Boolean)
 
 engine = sa.create_engine(settings.DB)
 session_factory = sessionmaker(bind = engine)
