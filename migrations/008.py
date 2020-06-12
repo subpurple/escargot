dac5aaf822e0bd95c4feb2fa73e1bdf350414181
walkingphasers <helloimtheguy312@hotmail.com>
2020-06-12 13:11:50 -0400

    fix requirements and migrations

--- migrations/008.py
+++ migrations/008.py
@@ -1,10 +1,11 @@
 from sqlaltery import ops
 import sqlalchemy as sa
+from util.json_type import JSONType
 
 OPS = [
 	ops.DropUnique('t_user', None),
 	ops.AddTable('t_login_token', (
-		sa.Column('data', util.json_type.JSONType(), nullable=False),
+		sa.Column('data', JSONType(), nullable=False),
 		sa.Column('expiry', sa.DateTime(), nullable=False),
 		sa.Column('id', sa.Integer(), nullable=False),
 		sa.Column('purpose', sa.String(), nullable=False),
