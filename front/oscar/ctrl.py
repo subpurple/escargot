2565b1dc5ebc8132bec2f52ba1006a2736866d93
subpurple <toxidation@msn.com>
2024-07-21 22:04:42 -0400

    aim: add rudimentary BUCP support with no authorization

--- front/oscar/ctrl.py
+++ front/oscar/ctrl.py
@@ -1,41 +1,162 @@
 import asyncio
-import struct
+import os
 import random
+import struct
+import settings
 
+from array import array
+from core.backend import Backend
+from dataclasses import dataclass
+from html import escape
 from typing import Optional, Any
 from util.misc import Logger
-from core.backend import Backend
+
+
+@dataclass
+class TLV:
+    type: int
+    data: bytes
+
+    def __init__(self, type: int, data: bytes):
+        self.type = type
+        self.data = data
+
+
+def decode_tlvs(data: bytes) -> list[TLV]:
+    tlvs = []
+
+    while len(data) > 4:
+        type, length = struct.unpack('>HH', data[0:4])
+        value = data[4:length + 4]
+
+        tlvs.append(TLV(type, value))
+        data = data[length + 4:]
+
+    return tlvs
+
+
+def encode_tlvs(tlvs: list[TLV]) -> bytes:
+    data = b''
+    for tlv in tlvs:
+        data += struct.pack('>HH', tlv.type, len(tlv.data)) + tlv.data
+
+    return data
+
+
+def find_tlv(tlvs: list[TLV], type: int):
+    for tlv in tlvs:
+        if tlv.type == type:
+            return tlv
+
+    return None
+
 
 class OSCARCtrl:
     logger: Logger
     transport: Optional[asyncio.WriteTransport]
-    backend: Backend
-    closed: bool
 
     sequence: int = random.randint(0x0000, 0xFFFF)
-	
-    def __init__(self, logger: Logger, backend: Backend) -> None:
+
+    def __init__(self, logger: Logger) -> None:
         self.logger = logger
         self.transport = None
-        self.backend = backend
-    
-    def on_connect(self) -> None:
-        self.send_reply(0x01, bytearray.fromhex('00 00 00 01'.replace(' ', '')))
-    
-    def data_received(self, frame: int, sequence: int, data: bytes) -> None:
-        self.logger.info('Frame:', frame)
-        self.logger.info('Sequence:', sequence)
-        self.logger.info('Data:', data.hex())
 
-    def send_reply(self, frame: int, data: bytes) -> None:
+    def send_specific_frame(self, frame: int, data: bytes) -> None:
         if self.sequence == 0xFFFF:
             self.sequence = 0x0000
         else:
             self.sequence += 1
-        
+
         packet = struct.pack('>BBHH', 0x2A, frame, self.sequence, len(data)) + data
-        self.logger.info('Sending:', packet.hex())
+        # self.logger.info('<<< Sending:', packet.hex())
         self.transport.write(packet)
+
+    # TODO(subpurple): include flags and request id
+    def send_snac(self, foodgroup: int, subgroup: int, data: bytes):
+        self.send_specific_frame(0x02, struct.pack('>HHHL', foodgroup, subgroup, 0, 0) + data)
+
+    def on_connect(self) -> None:
+        self.send_specific_frame(0x01, bytearray.fromhex('00 00 00 01'.replace(' ', '')))
+
+    def on_signon_frame(self, data: bytes) -> None:
+        # TODO(subpurple): support FLAP-level authentication aswell 
+        if len(data) > 4:
+            tlvs = decode_tlvs(data[4:])
+
+            # TODO(subpurple): check the BOS cookie
+            if find_tlv(tlvs, 0x0006):
+                self.logger.info('<<< OSERVICE__HOST_ONLINE')
+                self.send_snac(0x0001, 0x0003, struct.pack('>HH',
+                                                           0x0001,  # OSERVICE
+                                                           0x0005  # ADVERT
+                                                           ))
+                return
+
+    def on_data_frame(self, foodgroup: int, subgroup: int, flags: int, request_id: int, snac_data: bytes) -> None:
+        # TODO(subpurple): care about the flags and request id
+
+        match (foodgroup, subgroup):
+            case (0x0017, 0x0006):
+                self.logger.info('>>> BUCP__CHALLENGE_REQUEST')
+
+                key = str(int.from_bytes(os.urandom(4)))
+
+                self.logger.info('<<< BUCP__CHALLENGE_RESPONSE (key:', key + ')')
+                self.send_snac(0x0017, 0x0007, struct.pack('>H', 10) + key.encode())
+
+            case (0x0017, 0x0002):
+                self.logger.info('>>> BUCP_LOGIN_REQUEST')
+
+                tlvs = decode_tlvs(snac_data)
+
+                screen_name_tlv = find_tlv(tlvs, 0x0001)
+                hashed_pw_tlv = find_tlv(tlvs, 0x0025)
+
+                screen_name = screen_name_tlv.data.decode()
+
+                self.logger.info('>>> Screen Name:', screen_name)
+                self.logger.info('>>> Password (hashed):', hashed_pw_tlv.data)
+
+                email = f'{screen_name}@aol.com'
+                password_change_url = f'http://aim.aol.com/redirects/password/change_password.adp?ScreenName={escape(screen_name)}&ccode=us&lang=en'
+
+                # TODO(subpurple): actually authorize the user
+                if True:
+                    self.logger.info('<<< BUCP__LOGIN_RESPONSE (authorized)')
+                    self.send_snac(0x0017, 0x0003, encode_tlvs([
+                        TLV(0x0005, settings.TARGET_HOST.encode()),     # BOS address
+                        TLV(0x0006, os.urandom(256)),                   # BOS authorization cookie
+                        TLV(0x0011, email.encode()),                    # User's e-mail address
+                        TLV(0x0013, struct.pack('>H', 1)),              # Registration status
+                        TLV(0x0054, password_change_url.encode()),      # Password change URL
+                        TLV(0x008E, b'\0'),                             # Unknown
+                        TLV(0x0001, screen_name.encode())               # Screen name
+                    ]))
+                else:
+                    self.logger.info('<<< BUCP__LOGIN_RESPONSE (unauthorized)')
+
+                    error_code = 0x0001  # 0x0001 = Unregistered Screenname, 0x0005 = Mismatched Password
+                    error_url = \
+                        "http://www.aim.aol.com/errors/UNREGISTERED_SCREENNAME.html" \
+                            if error_code == 0x0001 \
+                            else "http://www.aim.aol.com/errors/MISMATCH_PASSWD.html"
+
+                    self.send_snac(0x0017, 0x0003, encode_tlvs([
+                        TLV(0x0008, struct.pack('>H', error_code)),
+                        TLV(0x0004, error_url.encode()),
+                        TLV(0x0001, screen_name.encode())
+                    ]))
+
+            case _:
+                self.logger.info('Recieved unknown SNAC with foodgroup:', hex(foodgroup), 'and subgroup:',
+                                 hex(subgroup))
+                self.logger.info('Data:', snac_data.hex())
+
+    def on_error_frame(self, data: bytes) -> None:
+        pass
+
+    def on_signoff_frame(self, data: bytes) -> None:
+        pass
 
     def close(self, **kwargs: Any) -> None:
         pass
--- front/oscar/entry.py
+++ front/oscar/entry.py
@@ -2,76 +2,96 @@ import asyncio
 import struct
 import settings
 
-from typing import Optional, Callable
-from util.misc import Logger
 from core.backend import Backend
 from .ctrl import OSCARCtrl
+from typing import Optional, Callable, Dict
 from threading import Thread
+from util.misc import Logger, ProtocolRunner
+
 
 def register(loop: asyncio.AbstractEventLoop, backend: Backend) -> None:
-	from util.misc import ProtocolRunner
-	
-	backend.add_runner(ProtocolRunner('0.0.0.0', 5190, ListenerOSCAR, args = ['OS', backend, OSCARCtrl]))
+    backend.add_runner(ProtocolRunner('0.0.0.0', 5190, ListenerOSCAR, args=['OS', OSCARCtrl]))
 
 
 class ListenerOSCAR(asyncio.Protocol):
-	logger: Logger
-	backend: Backend
-	controller: OSCARCtrl
-	transport: Optional[asyncio.WriteTransport]
+    logger: Logger
+    controller: OSCARCtrl
+    transport: Optional[asyncio.WriteTransport]
 
-	buffer: bytes = b''
-	data_thread: Thread = None
+    buffer: bytes = b''
+    data_thread: Thread = None
 
-	def __init__(self, logger_prefix: str, backend: Backend, controller_factory: Callable[[Logger, str, Backend], OSCARCtrl]) -> None:
-		super().__init__()
+    def __init__(self, logger_prefix: str, controller_factory: Callable[[Logger, str], OSCARCtrl]) -> None:
+        super().__init__()
 
-		self.logger = Logger(logger_prefix, self, settings.DEBUG_OSCAR)
-		self.backend = backend
-		self.controller = controller_factory(self.logger, backend)
-		self.controller.close_callback = self._on_close
+        self.logger = Logger(logger_prefix, self, settings.DEBUG_OSCAR)
+        self.controller = controller_factory(self.logger)
+        self.controller.close_callback = self._on_close
 
-	def connection_made(self, transport: asyncio.BaseTransport) -> None:
-		assert isinstance(transport, asyncio.WriteTransport)
+    def connection_made(self, transport: asyncio.BaseTransport) -> None:
+        self.controller.transport = transport
+        self.controller.on_connect()
+        self.logger.log_connect()
 
-		self.controller.transport = transport
-		self.logger.log_connect()
-		self.controller.on_connect()
-	
-	def connection_lost(self, exc: Optional[Exception]) -> None:
-		self.controller.close()
-		self.logger.log_disconnect()
-	
-	def data_received(self, packet: bytes) -> None:
-		if self.backend.maintenance_mode:	
-			self.transport.close()
-			return
+    def connection_lost(self, exc: Optional[Exception]) -> None:
+        # self.controller.close()
+        self.logger.log_disconnect()
 
-		self.buffer += packet
+    def data_received(self, packet: bytes) -> None:
+        self.buffer += packet
 
-		if self.data_thread is None or not self.data_thread.is_alive():
-			self.data_thread = Thread(target=self.parse_buffer)
-			self.data_thread.start()
+        if self.data_thread is None or not self.data_thread.is_alive():
+            self.data_thread = Thread(target=self.parse_buffer)
+            self.data_thread.start()
 
-	def parse_buffer(self) -> None:
-		while True:
-			if self.buffer[0] != 0x2A:
-				break
+    def parse_buffer(self) -> None:
+        while True:
+            if self.buffer[0] != 0x2A:
+                break
 
-			frame, sequence, length = struct.unpack('>BHH', self.buffer[1:6])
+            frame, sequence, length = struct.unpack('>BHH', self.buffer[1:6])
 
-			if len(self.buffer) < length + 6:
-				break
-			
-			if len(self.buffer) >= length + 6:
-				self.controller.data_received(frame, sequence, self.buffer[6:length + 6])
-				self.buffer = self.buffer[length + 6:]
+            if len(self.buffer) < length + 6:
+                break
 
-			# TODO(subpurple): This check might not be needed due to the "if len(self.buffer) < length + 6:" check above
-			if len(self.buffer) < 6:
-				break
+            if len(self.buffer) >= length + 6:
+                data = self.buffer[6:length + 6]
 
-	def _on_close(self) -> None:
-		if self.transport is None: 
-			return
-		self.transport.close()
+                match frame:
+                    # SIGNON
+                    case 0x01:
+                        self.controller.on_signon_frame(data)
+
+                    # DATA (always contains a SNAC)
+                    case 0x02:
+                        if len(data) < 10:
+                            return
+
+                        foodgroup, subgroup, flags, request_id = struct.unpack('>HHHL', data[:10])
+                        snac_data = data[10:]
+
+                        self.controller.on_data_frame(foodgroup, subgroup, flags, request_id, snac_data)
+
+                    # ERROR
+                    case 0x03:
+                        self.controller.on_error_frame(data)
+
+                    # SIGNOFF
+                    case 0x04:
+                        self.controller.on_signoff_frame(data)
+
+                    # KEEP_ALIVE
+                    case 0x05:
+                        pass
+
+                    case _:
+                        self.logger.info('Recieved unknown frame:', str(frame), 'with data:', data.hex())
+
+                self.buffer = self.buffer[length + 6:]
+
+            if len(self.buffer) < 6:
+                break
+
+    def _on_close(self) -> None:
+        if self.transport is not None:
+            self.transport.close()
